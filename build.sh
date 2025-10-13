5d60dcaed5974f6304c9eb0215e7a0eddc06fa99
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-10-13 21:30:31 +0200

    Ignore caching layer for kern conversion

--- build.sh
+++ build.sh
@@ -45,40 +45,36 @@ for url in $FILE_URLS; do
 	fi
 
 	# Convert to Humdrum/Kern if it doesn't exist or force-download is set
-	if [ ! -f "$KERN_DIR/$kern_file" ] || [ "$FORCE_DOWNLOAD" = true ]; then
-		echo "Converting $xml_file → $kern_file..."
+	echo "Converting $xml_file → $kern_file..."
 
-		# Add OPS and ONM based on filename
-		base="${filename%.mscx}"
-		OPS=$(echo "$base" | sed -E 's/op0*([0-9]+).*/\1/')
-		ONM=$(echo "$base" | sed -E 's/.*n0*([0-9]+).*/\1/')
-		OMV_LETTER=$(echo "$base" | sed -E 's/.*n[0-9]+([a-z]).*/\1/')
-		if [ -n "$OMV_LETTER" ]; then
-			OMV=$(( $(printf "%d" "'$OMV_LETTER") - 96 ))
-		else
-			OMV=0
-		fi
-
-		musicxml2hum "$MUSICXML_DIR/$xml_file" \
-			| grep -v '^55' \
-			| grep -v "*I'" \
-			| extractxx -I "**recip" \
-			| extractxx -I "**mxhm" \
-			| awk 'BEGIN{r=0} /Keyboard/ && r==0 {sub(/Keyboard/,"Organo"); r=1} {print}' \
-			| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violone"); r=1} {print}' \
-			| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violino II"); r=1} {print}' \
-			| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violino I"); r=1} {print}' \
-			| awk 'BEGIN{r=0} /OMV/ && r==0 {sub(/OMV/,"OMD"); r=1} {print}' \
-			| awk -v ops="$OPS" '/^!!!OTL/ {print; print "!!!OPS: " ops; next} {print}' \
-			| awk -v onm="$ONM" '/^!!!OPS/ {print; print "!!!ONM: " onm; next} {print}' \
-			| awk -v omv="$OMV" '/^!!!ONM/ {print; print "!!!OMV: " omv; next} {print}' \
-			| sed "s/!!!system-decoration:.*/!!!system-decoration: [{s1,s2},s3,s4]/g" \
-			| sed "s/!!!COM:.*/!!!COM: Corelli, Arcangelo/g" \
-			| echo -ne "$(cat)" \
-			| tee "$KERN_DIR/$kern_file" > /dev/null
+	# Add OPS and ONM based on filename
+	base="${filename%.mscx}"
+	OPS=$(echo "$base" | sed -E 's/op0*([0-9]+).*/\1/')
+	ONM=$(echo "$base" | sed -E 's/.*n0*([0-9]+).*/\1/')
+	OMV_LETTER=$(echo "$base" | sed -E 's/.*n[0-9]+([a-z]).*/\1/')
+	if [ -n "$OMV_LETTER" ]; then
+		OMV=$(( $(printf "%d" "'$OMV_LETTER") - 96 ))
 	else
-		echo "Using cached $kern_file"
+		OMV=0
 	fi
+
+	musicxml2hum "$MUSICXML_DIR/$xml_file" \
+		| grep -v '^55' \
+		| grep -v "*I'" \
+		| extractxx -I "**recip" \
+		| extractxx -I "**mxhm" \
+		| awk 'BEGIN{r=0} /Keyboard/ && r==0 {sub(/Keyboard/,"Organo"); r=1} {print}' \
+		| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violone"); r=1} {print}' \
+		| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violino II"); r=1} {print}' \
+		| awk 'BEGIN{r=0} /StringInstrument/ && r==0 {sub(/StringInstrument/,"Violino I"); r=1} {print}' \
+		| awk 'BEGIN{r=0} /OMV/ && r==0 {sub(/OMV/,"OMD"); r=1} {print}' \
+		| awk -v ops="$OPS" '/^!!!OTL/ {print; print "!!!OPS: " ops; next} {print}' \
+		| awk -v onm="$ONM" '/^!!!OPS/ {print; print "!!!ONM: " onm; next} {print}' \
+		| awk -v omv="$OMV" '/^!!!ONM/ {print; print "!!!OMV: " omv; next} {print}' \
+		| sed "s/!!!system-decoration:.*/!!!system-decoration: [{s1,s2},s3,s4]/g" \
+		| sed "s/!!!COM:.*/!!!COM: Corelli, Arcangelo/g" \
+		| echo -ne "$(cat)" \
+		| tee "$KERN_DIR/$kern_file" > /dev/null
 done
 
 echo "Done! Humdrum files: $KERN_DIR/"
